919006d49bc080acfe30b2287f1da2ff3795c73b
SkippyWalnuts <beccaearly89@gmail.com>
2023-10-03 12:57:45 +0100

    Added Dictionary component & form with functions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+let [word, setWord] = useState("");
+
+ 
+   function handleSubmit(event)  {
+    event.preventDefault();
+    alert(`'${word}' is a great word choice!`);
+}
+
+
+function logWord(event)  {
+    event.preventDefault();
+    setWord(event.target.value);
+}
+
+
     return (
         <div className="Dictionary">
-            <form>
-                <input type="search" placeholder="Enter a word to define"></input>
-                <input type="submit" value="Search"></input>
+            <form onSubmit={handleSubmit}>
+                <input type="search" 
+                placeholder="Enter your word here" 
+                autoFocus={true}
+                onChange={logWord}></input>
+                <input type="submit" value="Submit"></input>
             </form>
         </div>
     );
