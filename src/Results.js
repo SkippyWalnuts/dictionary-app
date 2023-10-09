64e8531acc93b24a58036e698885c345dc83f8d1
SkippyWalnuts <beccaearly89@gmail.com>
2023-10-09 18:15:25 +0100

    Added images, edited Results.css

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetics from "./Phonetics"
+import "./Results.css";
 
 export default function Results(props) {
     if (props.results) {
     return <div className="Results">
-           <h2><strong>{props.results.word}{": "}</strong></h2>
-           <Phonetics phonetic={props.results.phonetic}/>
+           <h2><strong>{props.results.word}</strong></h2>
+           
            {props.results.meanings.map(function(meaning, index) {
             return <div key={index}>
                 <Meaning meaning={meaning} />
             </div>;
            })}
+           <Phonetics phonetic={props.results.phonetic}/>
          </div>
    } else {
     return null;
