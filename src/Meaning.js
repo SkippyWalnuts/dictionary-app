6eda269def02b87ec16f1a41f209ddcf71e957e8
SkippyWalnuts <beccaearly89@gmail.com>
2023-10-11 10:14:13 +0100

    Edited css of Meaning elements & Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,9 +6,15 @@ export default function Meaning(props) {
       return (
         <div className="Meaning m-2">
             <h4>{props.partOfSpeech}</h4>
+            <div className="meaning-content px-4">
+       
            <p>{props.definition}</p>
+           
            <p><em>{props.example}</em></p>
+           
+           
             <Synonyms synonyms={props.synonyms}/>
-        </div>
+    </div>  
+    </div>
     );
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,12 +3,14 @@ import React from "react";
 export default function Synonyms(props) {
     if (props.synonyms) {
         return (
-            <div className="Synonyms">
-                <h4>AKA /<span className="akaExplanation"> Also Known As</span></h4>
+            <div className="Synonyms mb-3">
+                <hr />
+                <h5>AKA /<span className="akaExplanation"> Also Known As</span></h5>
+                <ul>
             {props.synonyms.map(function (synonym, index) {
-                return <span key={index}>{" / "}{synonym}</span>;
+                return <li key={index}>{synonym}</li>;
             })}
-            </div>    
+           </ul> </div>    
             );
          } else {
     return null;
